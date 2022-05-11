2c2968bdaadc346f7c49c458ccc06127fd8b6039
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-11 11:15:04 +0600

    collect data from user using booking modal

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -26,7 +26,11 @@ const AvailableAppointment = ({date}) => {
           />)
         }
       </div>
-      {treatment && <BookingModal treatment={treatment}></BookingModal>}
+      {treatment && <BookingModal 
+      date={date} 
+      treatment={treatment}
+      setTreatment={setTreatment}
+      ></BookingModal>}
     </div>
   );
 };
--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -1,24 +1,70 @@
+import { format } from 'date-fns';
 import React from 'react';
 
-const BookingModal = ({treatment}) => {
-  const {name, slots} = treatment;
+const BookingModal = ({ treatment, date, setTreatment }) => {
+  const { name, slots } = treatment;
+
+  // handle submit form
+  const handleBooking = event => {
+    event.preventDefault();
+    const slot = event.target.slot.value;
+    console.log(slot);
+    setTreatment(null)
+  }
+  
+  
+  
   return (
-    <div>
+    <div className="text-center">
       <input type="checkbox" id="booking-modal" class="modal-toggle" />
       <div class="modal modal-bottom sm:modal-middle">
         <div class="modal-box">
-        <label for="booking-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-          <h3 class="font-bold text-lg text-secondary">
+          <label
+            for="booking-modal"
+            class="btn btn-sm btn-circle absolute right-2 top-2"
+          >
+            ✕
+          </label>
+          <h3 class="font-bold text-lg text-secondary py-3">
             Booking for: {name}
           </h3>
-          <p class="py-4">
-           {slots}
-          </p>
-          <div class="modal-action">
-            <label for="booking-modal" class="btn">
-              Yay!
-            </label>
-          </div>
+          {/* form  */}
+          <form onSubmit={handleBooking} className="mx-auto w-full">
+            <input
+              type="text"
+              value={format(date, 'PP')}
+              disabled
+              class="input input-bordered w-full max-w-xs mb-2"
+            />
+            <select name='slot' class="select select-bordered w-full max-w-xs">
+              {
+                slots.map(slot=><option value={slot}> {slot} </option>)
+              }
+            </select>
+            <input
+              type="text"
+              name='name'
+              placeholder="Enter Your Name"
+              class="input input-bordered w-full max-w-xs mb-2"
+            />
+            <input
+              type="email"
+              name='email'
+              placeholder="Enter Your Email"
+              class="input input-bordered w-full max-w-xs mb-2"
+            />
+            <input
+              type="number"
+              name='phon'
+              placeholder="Enter Your phone number"
+              class="input input-bordered w-full max-w-xs mb-2"
+            />
+            <input
+              type="submit"
+              value="Submit"
+              class="btn btn-secondary w-full max-w-xs mb-2"
+            />
+          </form>
         </div>
       </div>
     </div>

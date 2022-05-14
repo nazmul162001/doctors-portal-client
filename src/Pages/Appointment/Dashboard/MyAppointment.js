38ed70a93a805a49abc769b600b06f4941f48efa
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-14 20:54:54 +0600

    added user booking dashboard

--- src/Pages/Appointment/Dashboard/MyAppointment.js
+++ src/Pages/Appointment/Dashboard/MyAppointment.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const MyAppointment = () => {
+  const [appoinments, setAppointments] = useState([]);
+  const [user] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      fetch(`http://localhost:5000/booking?patient=${user.email}`)
+        .then((res) => res.json())
+        .then((data) => setAppointments(data));
+    }
+  }, [user]);
   return (
     <div>
-      <h2>my appointment</h2>
+      <h2>Total Appointment: {appoinments.length} </h2>
+      <div class="overflow-x-auto">
+        <table class="table w-full">
+          {/* <!-- head --> */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Date</th>
+              <th>Time</th>
+              <th>Treatment</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* <!-- row 1 --> */}
+            {
+              appoinments.map((a, index) => <tr>
+                <th>{index + 1}</th>
+                <td> {a.patientName} </td>
+                <td>{a.date}</td>
+                <td>{a.slot}</td>
+                <td>{a.treatment}</td>
+              </tr>)
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -97,6 +97,24 @@ const Navbar = () => {
       <div className="navbar-center hidden lg:flex pr-0 md:pr-12">
         <ul className="menu menu-horizontal p-0">{menuItems}</ul>
       </div>
+      <div className="navbar-end">
+      <label tabIndex="1" for="dashboard-sidebar" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+      </div>
     </div>
   );
 };

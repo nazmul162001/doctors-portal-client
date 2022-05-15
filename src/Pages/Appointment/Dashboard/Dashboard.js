2e726560e87e3a995a2b895436104d99796b8646
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-15 15:30:46 +0600

    secure jwt token

--- src/Pages/Appointment/Dashboard/Dashboard.js
+++ src/Pages/Appointment/Dashboard/Dashboard.js
@@ -16,8 +16,9 @@ const Dashboard = () => {
     <label for="dashboard-sidebar" class="drawer-overlay"></label> 
     <ul class="menu p-4 overflow-y-auto w-48 bg-base-100 text-base-content">
       {/* <!-- Sidebar content here --> */}
-      <li><Link to='/dashboard' >My Dashboard</Link></li>
-      <li><Link to='/dashboard/review'>Review</Link></li>
+      <li><Link to='/dashboard' >My Appointment</Link></li>
+      <li><Link to='/dashboard/review'>My Reviews</Link></li>
+      <li><Link to='/dashboard/users'>All Users</Link></li>
     </ul>
   
   </div>
--- src/Pages/Appointment/Dashboard/MyAppointment.js
+++ src/Pages/Appointment/Dashboard/MyAppointment.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
+import {signOut} from 'firebase/auth';
 
 const MyAppointment = () => {
   const [appoinments, setAppointments] = useState([]);
   const [user] = useAuthState(auth);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (user) {
@@ -14,8 +17,19 @@ const MyAppointment = () => {
           'authorization': `Bearer ${localStorage.getItem('accessToken')}`
         }
       })
-        .then((res) => res.json())
-        .then((data) => setAppointments(data));
+        .then(res => {
+          console.log('res', res);
+          if(res.status === 401 || res.status === 403){
+            signOut(auth)
+            localStorage.removeItem('accessToken');
+            navigate('/')
+          }
+          return res.json()
+        })
+        .then((data) => {
+          setAppointments(data)
+
+        });
     }
   }, [user]);
   return (

08ebf0113083ab03861333a77105c560401505f4
nazmul162001 <nazmulprime162001@gmail.com>
2022-05-20 16:13:43 +0600

    get client secret

--- src/Pages/Appointment/Dashboard/CheckoutForm.js
+++ src/Pages/Appointment/Dashboard/CheckoutForm.js
@@ -1,53 +1,79 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const CheckoutForm = () => {
+const CheckoutForm = ({appointment}) => {
   const stripe = useStripe();
   const elements = useElements();
+  const [cardError, setCardError] = useState('');
+  const [clientSecret, setClientSecret] = useState('');
 
+  const {price} = appointment;
 
+  useEffect(()=> {
+    fetch('http://localhost:5000/create-payment-intent', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+      },
+      body: JSON.stringify({price})
 
-  const handleSubmit = async(e) => {
+    })
+    .then(res => res.json())
+    .then(data => {
+      if(data?.clientSecret){
+        setClientSecret(data.clientSecret);
+      }
+    })
+  },[price])
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if(!stripe || !elements){
+    if (!stripe || !elements) {
       return;
     }
     const card = elements.getElement(CardElement);
-    if(card === null){
+    if (card === null) {
       return;
     }
 
-    const {error, paymentMethod} = await stripe.createPaymentMethod({
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
       type: 'card',
       card,
     });
-    
 
-  }
-  
-  
+    setCardError(error?.message || '');
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: '16px',
-              color: '#424770',
-              '::placeholder': {
-                color: '#aab7c4',
+    <>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: '16px',
+                color: '#424770',
+                '::placeholder': {
+                  color: '#aab7c4',
+                },
+              },
+              invalid: {
+                color: '#9e2146',
               },
             },
-            invalid: {
-              color: '#9e2146',
-            },
-          },
-        }}
-      />
-      <button className='btn btn-sm mt-4 px-5' type="submit" disabled={!stripe}>
-        Pay
-      </button>
-    </form>
+          }}
+        />
+        <p className="italic text-red-500 mt-4"> {cardError} </p>
+        <button
+          className="btn btn-sm mt-4 px-5"
+          type="submit"
+          disabled={!stripe || !clientSecret}
+        >
+          Pay
+        </button>
+      </form>
+    </>
   );
 };
 
